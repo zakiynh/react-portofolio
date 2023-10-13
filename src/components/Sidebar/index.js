e41e98a0d33b044b4dc3a1a505375e38cda16113
zakiy <zakiynurhasyim@datindo.co.id>
2023-10-13 14:02:08 +0700

    Add components Sidebar

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,65 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoZ from '../../assets/images/Z-Logos.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faFacebook, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoZ} alt='Z-Logos' />
+        </Link>
+        <nav>
+            <NavLink exact="true" activeClassName="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeClassName="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+            </NavLink>
+            <NavLink exact="true" activeClassName="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+            </NavLink>
+        </nav>
+        <ul>
+            <li>
+                <a
+                    target='_blank'
+                    rel='noreferrer'
+                    href='https://www.linkedin.com/in/zakiynh/'
+                >
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a
+                    target='_blank'
+                    rel='noreferrer'
+                    href='https://github.com/zakiynh'
+                >
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a
+                    target='_blank'
+                    rel='noreferrer'
+                    href='https://www.instagram.com/zakiynh/'
+                >
+                    <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a
+                    target='_blank'
+                    rel='noreferrer'
+                    href='https://www.facebook.com/mzakiyn/'
+                >
+                    <FontAwesomeIcon icon={faFacebook} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar

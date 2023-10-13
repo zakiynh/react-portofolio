a178acd4fd578abc1b1fe10d99f2993565a2835f
zakiy <zakiynurhasyim@datindo.co.id>
2023-10-13 20:26:03 +0700

    add home layout

---
+++ src/components/Home/index.js
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+import './index.scss'
+
+const Home = () => {
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi,
+                    <br />
+                    I'm Zakiy
+                    <br />
+                    web developer
+                </h1>
+                <h2>Frontend Developer</h2>
+                <Link to='/contact' className="flat-button">CONTACT ME</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Home

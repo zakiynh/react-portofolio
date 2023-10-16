a7fd63899844c4b1a51512962e8fe1890c1076f4
zakiy <zakiynurhasyim@datindo.co.id>
2023-10-16 10:00:54 +0700

    finish home-text with animated

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,38 @@
 import { Link } from "react-router-dom"
 import './index.scss'
+import AnimatedLetters from "../AnimatedLetters"
+import { useEffect, useState } from "react"
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['Z', 'a', 'k', 'i', 'y']
+    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+
+    // useEffect(() => {
+    //     return setTimeout(() => {
+    //         setLetterClass('text-animate-hover')
+    //     }, 4000)
+    // }, [])
+
+    useEffect(() => {
+        const timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000);
+        return () => clearTimeout(timeoutId); // Cleanup the timeout
+    }, [])
+
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi,
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i</span>
                     <br />
-                    I'm Zakiy
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`} style={{ margin: '0 10px 0 0' }}>'m</span>
+                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
                     <br />
-                    web developer
+                    <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={20} />
                 </h1>
                 <h2>Frontend Developer</h2>
                 <Link to='/contact' className="flat-button">CONTACT ME</Link>
